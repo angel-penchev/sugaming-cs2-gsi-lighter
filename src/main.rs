d5841cf62e6136f18bf7cc686d264ece1203e913
Angel Penchev <angel.penchev.tsb@gmail.com>
2024-10-11 17:59:23 +0300

    feat: cs2 gsi init
    Initialized CS2 GSI listening in the project.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use gsi_cs2::Body;
+use poem::{handler, listener::TcpListener, post, web::Json, Route, Server};
+
+#[handler]
+fn update(data: Json<Body>) {
+    println!("{:#?}", data);
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    tracing_subscriber::fmt::init();
+
+    let app = Route::new().at("/", post(update));
+
+    Server::new(TcpListener::bind("127.0.0.1:3000"))
+        .run(app)
+        .await
 }
